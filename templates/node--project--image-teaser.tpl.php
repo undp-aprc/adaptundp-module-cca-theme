ae15088b5b8234523e017a4e2c2919b38f2a7b59
Matt Clark <matthew.clark@undp.org>
2016-03-17 17:36:54 +1100

    Changed project header size

--- templates/node--project--image-teaser.tpl.php
+++ templates/node--project--image-teaser.tpl.php
@@ -2,8 +2,8 @@
     <a href="/node/<?php print($node->nid); ?>">
         <div class="cca-content-box photo-box caption-bottom primary">
             <?php print render($content['field_display_photo']); ?>
-            <h2><?php print($variables['title']); ?></h2>
-            <?php //print render($content['body']); ?>
+            <h4><?php print($variables['title']); ?></h4>
+            <?php print render($content['body']); ?>
         </div>
     </a>
     </a>
