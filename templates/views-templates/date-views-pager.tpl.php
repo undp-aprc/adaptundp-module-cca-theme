6dc00accdacfb6389699c559a79939312318bdae
Matt Clark <matthew.clark@undp.org>
2016-12-05 16:03:42 +1100

    Updated CSS to override default date pager styling

--- templates/views-templates/date-views-pager.tpl.php
+++ templates/views-templates/date-views-pager.tpl.php
@@ -31,16 +31,16 @@
 <div class="date-nav-wrapper clearfix<?php if (!empty($extra_classes)) print $extra_classes; ?>">
     <div class="date-nav item-list">
         <div class="date-heading">
-            <h3><?php print $nav_title ?></h3>
+            <p><?php print $nav_title ?></p>
         </div>
         <ul class="pager">
             <?php if (!empty($prev_url)) : ?>
-                <li class="date-prev">
+                <li class="date-nav-button date-prev">
                     <?php print l('&laquo;' . ($mini ? '' : ' ' . t(array(), array('context' => 'date_nav'))), $prev_url, $prev_options); ?>
                     </li>
             <?php endif; ?>
             <?php if (!empty($next_url)) : ?>
-                <li class="date-next">
+                <li class="date-nav-button date-next">
                     <?php print l(($mini ? '' : t(array(), array('context' => 'date_nav')) . ' ') . '&raquo;', $next_url, $next_options); ?>
                 </li>
             <?php endif; ?>
