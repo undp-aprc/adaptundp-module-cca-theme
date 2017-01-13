5e411bd75f0dbd38a0a1ce80675781c019df4aad
Matt Clark <matthew.clark@undp.org>
2017-01-13 14:22:15 +1100

    Working on project template

--- templates/nodes/project/node--project--synopsis.tpl.php
+++ templates/nodes/project/node--project--synopsis.tpl.php
@@ -5,7 +5,7 @@
         <h4>Project Overview</h4>
         <div class="row">
             <div class="project-summary col-xs-8">
-                <?php print(render($content['field_project_about_summary'])); ?>
+                <?php print(render($content['body'])); ?>
             </div>
             <div class="map col-xs-4">
                 <div class="panel panel-default">
